dbfcde8c876b1c328c28c1170e16c0c9f7a85006
jimmyjames88 <me@james-allen.ca>
2024-02-09 21:49:43 -0700

    Expense class tweak

--- src/classes/Expense.ts
+++ src/classes/Expense.ts
@@ -1,7 +1,7 @@
 import { _Expense, _Tax } from '@jimmyjames88/freebooks-types'
 
 export default class Expense implements _Expense {
-  public id!: number
+  public id?: number
   public UserId!: number
   public InvoiceId?: number
   public paymentTypeId!: number
