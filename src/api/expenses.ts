cbfe571f7f1f2389a4b9e36f8bcfb2234a28e319
jimmyjames88 <me@james-allen.ca>
2024-09-11 17:38:31 -0600

    added 'unattached' filter to expenses api index

--- src/api/expenses.ts
+++ src/api/expenses.ts
@@ -1,11 +1,15 @@
 import axios from 'axios'
-import { _Collection, _DataTableFilters } from '@jimmyjames88/freebooks-types'
+import { _Collection, _DataTableFilters, _Expense } from '@jimmyjames88/freebooks-types'
 import { Expense } from '@/classes'
 
 const url = `${import.meta.env.VITE_API}/expenses`
 
+interface _ExpensesFilters extends _DataTableFilters {
+  unattached?: boolean
+}
+
 export default {
-  async index(params: _DataTableFilters): Promise<_Collection<Expense>> { 
+  async index(params: _ExpensesFilters): Promise<_Collection<Expense>> { 
     const response = await axios.get(`${url}`, { params })
     return {
       items: response.data.items.map((expense: Expense) => new Expense(expense)),
