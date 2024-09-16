ee65602b83afdf8d62052c517f283ea2654ae689
jimmyjames88 <me@james-allen.ca>
2024-09-15 18:35:41 -0600

    Refactor Invoice composable: no longer instantiates an Invoice object on creation, instead will depend on clearInvoice and loadInvoice methods for class instantiation

--- src/composables/Invoice.ts
+++ src/composables/Invoice.ts
@@ -1,25 +1,25 @@
 import { computed, ref } from 'vue'
 import { _InvoiceStatus, _Tax, _TaxType } from '@jimmyjames88/freebooks-types'
 import API from '@/api'
-import { Client, Expense, Invoice as InvoiceClass, User } from '@/classes'
+import { Expense, Invoice as InvoiceClass } from '@/classes'
 
-
-const Invoice = ref<InvoiceClass>(new InvoiceClass({}))
-Invoice.value = new InvoiceClass(initialState)
+const Invoice = ref<InvoiceClass>()
 
 export default function InvoiceComposable() {
 
-  const resetInvoice = () => {
-    Invoice.value = new InvoiceClass(Object.assign(initialState, []))
+  // Replaces instance of Invoice with a new, empty instance
+  const clearInvoice = () => {
+    Invoice.value = new InvoiceClass({})
   }
 
+  // Load an invoice from the API and set it to the Invoice ref
   const loadInvoice = async (id: number) => {
     const invoice = await API.invoices.show(id)
     Invoice.value = invoice
   }
 
   const expensesTotal = computed(() => {
-    return Invoice.value.Expenses?.reduce((acc: number, expense: Expense) => {
+    return Invoice.value?.Expenses?.reduce((acc: number, expense: Expense) => {
       const tax = expense.Taxes?.reduce((acc: number, tax: _Tax) => {
         return acc + (tax.type === _TaxType.PERCENTAGE ? (expense.subtotal * tax.rate) : tax.rate)
       }, 0) || 0
@@ -28,13 +28,13 @@ export default function InvoiceComposable() {
   })
 
   const subtotal = computed(() => {
-    return Invoice.value.lineItems?.reduce((acc: number, item) => {
+    return Invoice.value?.LineItems?.reduce((acc: number, item) => {
       return acc + ((item.rate || 0) * (item.quantity || 1))
-    }, 0)
+    }, 0) || 0
   })
 
   const tax = computed(() => {
-    return Invoice.value.Taxes?.reduce((acc: number, tax: _Tax) => {
+    return Invoice.value?.Taxes?.reduce((acc: number, tax: _Tax) => {
       return acc + (tax.type === _TaxType.PERCENTAGE ? (subtotal.value * tax.rate) : tax.rate)
     }, 0) || 0
   })
@@ -44,18 +44,18 @@ export default function InvoiceComposable() {
   })
 
   const isSent = computed(() => {
-    return Invoice.value.status === _InvoiceStatus.DRAFT
+    return Invoice.value?.status === _InvoiceStatus.DRAFT
   })
 
   const pastDue = computed(() => {
-    if (!Invoice.value.dueDate) return false
+    if (!Invoice.value?.dueDate) return false
     const dueDate = new Date(Invoice.value.dueDate)
     const today = new Date()
     return dueDate < today
   })
 
   const paymentsTotal = computed(() => {
-    return Invoice.value.Payments?.reduce((acc, payment) => {
+    return Invoice.value?.Payments?.reduce((acc, payment) => {
       return Number(acc + +payment.amount)
     }, 0) || 0
   })
@@ -72,7 +72,7 @@ export default function InvoiceComposable() {
     loadInvoice,
     pastDue,
     paymentsTotal,
-    resetInvoice,
+    clearInvoice,
     subtotal,
     tax,
     total
