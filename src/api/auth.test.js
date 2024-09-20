2a201e6aa66c1d7f58dc4c8e91447e99f80e9826
jimmyjames88 <me@james-allen.ca>
2024-09-19 19:36:19 -0600

    formatting fixes and implemented host env variable for api endpoint references

--- src/api/auth.test.js
+++ src/api/auth.test.js
@@ -1,69 +1,71 @@
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import { describe, it, expect, beforeEach } from 'vitest';
-import API from '../api';
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { describe, it, expect, beforeEach } from 'vitest'
+import API from '../api'
+
+const host = import.meta.env.VITE_AUTH_API
 
 describe('auth.ts', () => {
-  let mock;
+  let mock
 
   beforeEach(() => {
     // Create a new instance of MockAdapter for each test
-    mock = new MockAdapter(axios);
-  });
+    mock = new MockAdapter(axios)
+  })
 
   // Test login method
   it('login method uses the correct endpoint and receives a status of 200', async () => {
-    const url = 'http://192.168.0.3:3000/auth/login'
-    mock.onPost(url).reply(200, { status: 200 });
+    const url = `${host}/auth/login`
+    mock.onPost(url).reply(200, { status: 200 })
 
-    const response = await API.auth.login('test', 'test');
+    const response = await API.auth.login('test', 'test')
     console.log('#######', response)
-    expect(response.status).toBe(200);
-    expect(mock.history.post.length).toBe(1);
-    expect(mock.history.post[0].url).toBe(url);
+    expect(response.status).toBe(200)
+    expect(mock.history.post.length).toBe(1)
+    expect(mock.history.post[0].url).toBe(url)
     console.log('#######', mock.history.post)
     expect(JSON.parse(mock.history.post[0].data)).toEqual({
       email: 'test',
       password: 'test'
-    });
-  });
+    })
+  })
 
   // Test checkEmail method
   it('checkEmail method uses the correct endpoint and receives a status of 200', async () => {
-    const url = 'http://192.168.0.3:3000/auth/check-email';
-    mock.onPost(url).reply(200, { status: 200 });
+    const url = `${host}/auth/check-email`
+    mock.onPost(url).reply(200, { status: 200 })
   
-    const response = await API.auth.checkEmail('me@james-allen.ca');
+    const response = await API.auth.checkEmail('me@james-allen.ca')
   
-    expect(response.status).toBe(200);
-    expect(mock.history.post.length).toBe(1);
-    expect(mock.history.post[0].url).toBe(url);
-    expect(JSON.parse(mock.history.post[0].data)).toEqual({ email: 'me@james-allen.ca' });
-  });
+    expect(response.status).toBe(200)
+    expect(mock.history.post.length).toBe(1)
+    expect(mock.history.post[0].url).toBe(url)
+    expect(JSON.parse(mock.history.post[0].data)).toEqual({ email: 'me@james-allen.ca' })
+  })
 
   // Test register method handles successful response
   it('register method handles successful response', async () => {
     // Mock the axios POST request to return a 200 status
-    const url = 'http://192.168.0.3:3000/auth/register'
+    const url = `${host}/auth/register`
     mock.onPost(url).reply(200, {
       status: 200,
       message: 'Registration successful'
-    });
+    })
 
     const response = await API.auth.register({
       email: 'valid-email@example.com',
       password: 'validpassword',
       name: 'James Allen'
-    });
+    })
 
-    expect(response.status).toBe(200);
-    expect(response.data.message).toBe('Registration successful');
-    expect(mock.history.post.length).toBe(1);
-    expect(mock.history.post[0].url).toBe(url);
+    expect(response.status).toBe(200)
+    expect(response.data.message).toBe('Registration successful')
+    expect(mock.history.post.length).toBe(1)
+    expect(mock.history.post[0].url).toBe(url)
     expect(JSON.parse(mock.history.post[0].data)).toEqual({
       email: 'valid-email@example.com',
       password: 'validpassword',
       name: 'James Allen'
-    });
-  });
-});
+    })
+  })
+})
